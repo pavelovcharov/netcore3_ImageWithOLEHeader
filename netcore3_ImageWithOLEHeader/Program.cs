aaf80442a7e7f0439d53bf2475a1491f5423d1f9
Ovcharov.Pavel <1357165+pavelovcharov@users.noreply.github.com>
2019-03-28 13:29:45 +0300

    init

---
+++ netcore3_ImageWithOLEHeader/Program.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace netcore3_ImageWithOLEHeader {
+
+    [TestFixture]
+    public class TestClass {
+        [Test]
+        public void ImageWithOLEHeader() {
+            var type = GetType();
+            var assembly = type.Assembly;
+            using(var imageStream = assembly.GetManifestResourceStream(assembly.GetName().Name + ".NorthwindDBImage.bmp")) {
+                using(var ms = new MemoryStream()) {
+                    imageStream.CopyTo(ms);
+                    var converter = new ImageConverter();
+                    var image = converter.ConvertFrom(ms.ToArray());
+                    Assert.IsNotNull(image);
+                }
+            }
+        }
+    }
+}
